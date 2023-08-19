8d7b92a0750452a5f7b67cac53093e35ef9cb28e
Artchem <chemerys.artem@gmail.com>
2023-08-20 00:12:11 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,36 +1,99 @@
 import * as API from './js/pixabay-api';
-// import SlimSelect from 'slim-select';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
   loadMore: document.querySelector('.load-more'),
-  //   error: document.querySelector('.error'),
 };
+let pageCurrent = 1;
+let searchQuery = '';
+let gallery = new SimpleLightbox('.gallery-link');
 
 refs.searchForm.addEventListener('submit', onSearchSubmit);
 refs.loadMore.addEventListener('click', onLoadMoreClick);
-refs.loadMore.disabled = true;
 
-function onLoadMoreClick() {}
+function onLoadMoreClick(evt) {
+  console.dir(refs.searchForm.elements.searchQuery.value);
+  pageCurrent += 1;
 
-function onSearchSubmit(evt) {
-  evt.preventDefault();
-  const searchQuery = evt.currentTarget.elements.searchQuery.value;
-  console.log(searchQuery);
+  refs.loadMore.disabled = true;
+  refs.loadMore.textContent = 'Loading...';
 
-  API.fetchPixabay(searchQuery)
+  API.fetchPixabay(searchQuery, pageCurrent)
     .then(data => {
       console.log(data);
-      refs.gallery.innerHTML = createMarkup(data.hits);
+
+      refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+
+      gallery.refresh();
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight,
+        behavior: 'smooth',
+      });
+
+      refs.loadMore.disabled = false;
+      refs.loadMore.textContent = 'Load more';
+
+      console.log(pageCurrent * data.hits.length);
+      if (pageCurrent * data.hits.length >= data.totalHits) {
+        refs.loadMore.hidden = true;
+      }
 
       if (data.hits.length === 0) {
         Notiflix.Notify.failure(
           `Sorry, there are no images matching your search query. Please try again.`
         );
       }
-      refs.loadMore.disabled = false;
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
+}
+
+function onSearchSubmit(evt) {
+  evt.preventDefault();
+  searchQuery = evt.currentTarget.elements.searchQuery.value;
+  console.log(searchQuery);
+
+  refs.gallery.innerHTML = '';
+  pageCurrent = 1;
+
+  API.fetchPixabay(searchQuery, pageCurrent)
+    .then(data => {
+      console.log(data.hits);
+
+      refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+
+      gallery = new SimpleLightbox('.gallery-link');
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        behavior: 'smooth',
+      });
+
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+      if (data.hits.length !== data.totalHits) {
+        refs.loadMore.hidden = false;
+      }
+
+      if (data.hits.length === 0) {
+        Notiflix.Notify.failure(
+          `Sorry, there are no images matching your search query. Please try again.`
+        );
+      }
+      // refs.loadMore.disabled = false;
       // refs.breedSelect.classList.remove('is-hidden');
       // refs.loader.classList.add('is-hidden');
       // new SlimSelect({
@@ -47,70 +110,26 @@ function onSearchSubmit(evt) {
       // });
 
       // refs.loader.classList.add('is-hidden');
-    });
+    })
+    .finally(evt.currentTarget.reset());
 }
-
-// function pushDataInSelect(array) {
-//   return array
-//     .map(({ id, name }) => `<option  value="${id}">${name}</option>`)
-//     .join('');
-// }
-
-// refs.breedSelect.addEventListener('change', onBreedClick);
-
-// function onBreedClick(evt) {
-//   const searchBread = evt.target.value;
-
-//   refs.loader.classList.remove('is-hidden');
-
-//   refs.catInfo.innerHTML = '';
-
-//   API.fetchCatByBreed(searchBread)
-//     .then(data => {
-//       if (data.length === 0) {
-//         Notiflix.Notify.failure(
-//           `Sorry, there are no images matching your search query. Please try again.`,
-//           {
-//             width: '380px',
-//             position: 'center-center',
-//           }
-//         );
-//         console.log('Такого кота немає');
-//       }
-//       refs.catInfo.innerHTML = createMarkup(data);
-//       refs.loader.classList.add('is-hidden');
-//     })
-//     .catch(error => {
-//       console.dir(error.message);
-//       Notiflix.Notify.failure(
-//         `Oops! ${error.message}! Try reloading the page!`,
-//         {
-//           width: '380px',
-//           position: 'center-center',
-//           timeout: 6000,
-//           clickToClose: true,
-//         }
-//       );
-//       // refs.error.classList.remove('is-hidden');
-//       refs.loader.classList.add('is-hidden');
-//       // refs.breedSelect.classList.add('is-hidden');
-//     });
-// }
-
-// webformatURL -  посилання на маленьке зображення для списку карток.
-// largeImageURL - посилання на велике зображення.
-// tags -          рядок з описом зображення. Підійде для атрибуту alt.
-// likes -        кількість лайків.
-// views -       кількість переглядів.
-// comments -   кількість коментарів.
-// downloads -    кількість завантажень.
 
 function createMarkup(arr) {
   return arr
     .map(
-      ({ webformatURL, tags, likes, views, comments, downloads }) =>
+      ({
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+        largeImageURL,
+      }) =>
         `<div class="photo-card">
-            <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300" />
+            <a class = "gallery-link" href= "${largeImageURL}">
+            <img class="cover" src="${webformatURL}" alt="${tags}" loading="lazy" width="300" height="200"/>
+            </a> 
              <div class="info">
                 <p class="info-item">
                   <b>Likes</b>${likes}
@@ -125,6 +144,7 @@ function createMarkup(arr) {
                   <b>Downloads</b>${downloads}
                 </p>
               </div>
+            
           </div>`
     )
     .join('');
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,14 +4,14 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 // axios.defaults.headers.common['key'] = '38837496-e09cca1b216ed759136fb60be';
 const KEY = '38837496-e09cca1b216ed759136fb60be';
 
-function fetchPixabay(value) {
+function fetchPixabay(value, page = 1) {
   return axios
     .get(
-      `?key=${KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
+      `?key=${KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
     )
 
     .then(response => {
-      //   console.log(response);
+      console.log(response);
       return response.data;
     });
 }
